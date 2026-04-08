c6af442695a8fb7cb006f0a07a652583e43bbe14
arabescripts-123 <arabescripts@gmail.com>
2026-04-08 20:52:04 -0300

    Update ScriptDumper_Solara.lua

--- ScriptDumper_Solara.lua
+++ ScriptDumper_Solara.lua
@@ -1,31 +1,8 @@
--- FIXED VERSION (SEM ERRO)
-
-local player = game.Players.LocalPlayer
-local char = player.Character or player.CharacterAdded:Wait()
-
-local root = char:WaitForChild("HumanoidRootPart")
-
-for _,plr in pairs(game.Players:GetPlayers()) do
-    if plr ~= player then
-        
-        local targetChar = plr.Character
-        if targetChar then
-            
-            local hrp = targetChar:FindFirstChild("HumanoidRootPart")
-
-            if hrp then
-                local distance = (hrp.Position - root.Position).Magnitude
-
-                if distance < 10 then
-                    local direction = (hrp.Position - root.Position)
-
-                    if direction.Magnitude > 0 then
-                        direction = direction.Unit
-
-                        hrp.AssemblyLinearVelocity = direction * 300 + Vector3.new(0,150,0)
-                    end
-                end
-            end
+for _,v in pairs(getgc(true)) do
+    if typeof(v) == "function" then
+        local info = debug.getinfo(v)
+        if info.name and info.name:lower():find("kick") then
+            print("FUNÇÃO SUSPEITA:", info.name)
         end
     end
 end
